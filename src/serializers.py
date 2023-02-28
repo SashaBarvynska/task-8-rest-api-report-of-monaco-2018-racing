1918413d7ba50bc9fbba1cbafa283694a0e9b3fc
Sasha Barvynska <alexbarvynska@gmail.com>
2023-02-28 12:34:36 +0200

    changed type response in func data_to_json and data_to_xml

--- src/serializers.py
+++ src/serializers.py
@@ -1,9 +1,9 @@
 from task_Barvynska import Driver
 
 
-def data_to_json(list_drivers: list[Driver]) -> list[dict[str, str]]:
+def data_to_json(list_drivers: list[Driver]) -> list[Driver]:
     return [{"abbreviation": x.abbreviation, "driver": x.driver} for x in list_drivers]
 
 
-def data_to_xml(list_drivers: list[Driver]) -> list[dict[str, str]]:
+def data_to_xml(list_drivers: list[Driver]) -> list[Driver]:
     return [x.__dict__ for x in list_drivers]
